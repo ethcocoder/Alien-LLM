c7aad726f2a702865e2c4a46ded3dfeccc2d5605
ethcocoder <natnaelermiyas461@gmail.com>
2026-06-27 09:43:35 +0100

    build faild solve

--- include/rfa.hpp
+++ include/rfa.hpp
@@ -52,12 +52,15 @@ public:
     void update(const Eigen::VectorXf& q, const Eigen::VectorXf& grad_out, float lr = 0.001f) {
         t++;
         Eigen::VectorXf phi_q = feature_map(q);
-        Eigen::MatrixXf grad = grad_out * phi_q.transpose();
+        // Map d_model grad to r_features grad (approximate)
+        Eigen::VectorXf grad_phi = (W_random * grad_out).cwiseProduct(phi_q);
+        Eigen::MatrixXf grad = grad_phi * q.transpose();
         
         // Adam Update
         float b1 = 0.9f, b2 = 0.999f, eps = 1e-8f;
         m_W = b1 * m_W + (1.0f - b1) * grad;
         v_W = b2 * v_W + (1.0f - b2) * grad.array().square().matrix();
+
         
         Eigen::MatrixXf m_hat = m_W / (1.0f - std::pow(b1, t));
         Eigen::MatrixXf v_hat = v_W / (1.0f - std::pow(b2, t));
